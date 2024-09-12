781794e2e5ac3dbda059e598601c4447031ab984
Admin <tgr1196@wgu.edu>
2024-09-12 10:15:57 -0700

    Write code for the entities package that includes entity classes and the enum designed to match the UML diagram. Added proper mapping to each

---
+++ src/main/java/entities/Division.java
@@ -0,0 +1,45 @@
+package entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name="divisions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Division {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "division_id")
+    private Long id;
+
+    @Column(name = "division")
+    private String division_name;
+
+    @CreationTimestamp
+    @Column(name = "create_date")
+    private Date createDate;
+
+    @UpdateTimestamp
+    @Column(name = "last_update")
+    private Date last_update;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @Column(name = "country_id", insertable = false, updatable = false)
+    private Long countryID;
+
+    @OneToMany(mappedBy = "division", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Customer> customers;
+}
