a0dcea60e1f110fb7cb9574514b11749066ff720
Admin <tgr1196@wgu.edu>
2024-09-25 18:12:40 -0700

    Initial Commit. working_branch2. first working-branch was corrupt.

---
+++ src/test/java/com/d288/bookings/BookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d288.bookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
